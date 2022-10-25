b2593c7ac2e5d7d6278a2a5b27cb02b934ace6fe
aeither <36173828+aeither@users.noreply.github.com>
2022-10-25 13:49:24 +0200

    update idl

--- src/utils/idl.ts
+++ src/utils/idl.ts
@@ -3,211 +3,76 @@ export type Dungeon3 = {
   name: "dungeon3";
   instructions: [
     {
-      name: "setUser";
+      name: "addItem";
       accounts: [
-        {
-          name: "authority";
-          isMut: true;
-          isSigner: true;
-        },
-        {
-          name: "userAccount";
-          isMut: true;
-          isSigner: false;
-        }
+        { name: "authority"; isMut: true; isSigner: true },
+        { name: "itemAccount"; isMut: true; isSigner: false }
       ];
-      args: [
-        {
-          name: "score";
-          type: "u64";
-        },
-        {
-          name: "map";
-          type: "u8";
-        },
-        {
-          name: "health";
-          type: "u8";
-        }
-      ];
+      args: [];
     },
     {
       name: "initUser";
       accounts: [
-        {
-          name: "authority";
-          isMut: true;
-          isSigner: true;
-        },
-        {
-          name: "newUserAccount";
-          isMut: true;
-          isSigner: false;
-          pda: {
-            seeds: [
-              {
-                kind: "const";
-                type: "string";
-                value: "user";
-              },
-              {
-                kind: "account";
-                type: "publicKey";
-                path: "authority";
-              }
-            ];
-          };
-        },
-        {
-          name: "rent";
-          isMut: false;
-          isSigner: false;
-        },
-        {
-          name: "systemProgram";
-          isMut: false;
-          isSigner: false;
-        }
-      ];
-      args: [];
-    },
-    {
-      name: "addItem";
-      accounts: [
-        {
-          name: "authority";
-          isMut: true;
-          isSigner: true;
-        },
-        {
-          name: "itemAccount";
-          isMut: true;
-          isSigner: false;
-        }
-      ];
-      args: [];
-    },
-    {
-      name: "exportItems";
-      accounts: [
-        {
-          name: "authority";
-          isMut: true;
-          isSigner: true;
-        },
-        {
-          name: "itemAccount";
-          isMut: true;
-          isSigner: false;
-        }
+        { name: "authority"; isMut: true; isSigner: true },
+        { name: "newUserAccount"; isMut: true; isSigner: false },
+        { name: "systemProgram"; isMut: false; isSigner: false },
+        { name: "rent"; isMut: false; isSigner: false }
       ];
       args: [];
     },
     {
       name: "initItem";
       accounts: [
-        {
-          name: "authority";
-          isMut: true;
-          isSigner: true;
-        },
-        {
-          name: "newItemAccount";
-          isMut: true;
-          isSigner: false;
-          pda: {
-            seeds: [
-              {
-                kind: "const";
-                type: "string";
-                value: "item";
-              },
-              {
-                kind: "account";
-                type: "publicKey";
-                path: "authority";
-              },
-              {
-                kind: "arg";
-                type: "u8";
-                path: "id";
-              }
-            ];
-          };
-        },
-        {
-          name: "rent";
-          isMut: false;
-          isSigner: false;
-        },
-        {
-          name: "systemProgram";
-          isMut: false;
-          isSigner: false;
-        }
+        { name: "authority"; isMut: true; isSigner: true },
+        { name: "newItemAccount"; isMut: true; isSigner: false },
+        { name: "systemProgram"; isMut: false; isSigner: false },
+        { name: "rent"; isMut: false; isSigner: false }
+      ];
+      args: [{ name: "id"; type: "u8" }];
+    },
+    {
+      name: "setUser";
+      accounts: [
+        { name: "authority"; isMut: true; isSigner: true },
+        { name: "userAccount"; isMut: true; isSigner: false }
       ];
       args: [
-        {
-          name: "id";
-          type: "u8";
-        }
+        { name: "score"; type: "u64" },
+        { name: "map"; type: "u8" },
+        { name: "health"; type: "u8" }
       ];
+    },
+    {
+      name: "exportItems";
+      accounts: [
+        { name: "authority"; isMut: true; isSigner: true },
+        { name: "itemAccount"; isMut: true; isSigner: false }
+      ];
+      args: [];
     }
   ];
   accounts: [
     {
-      name: "userAccount";
+      name: "ItemAccount";
       type: {
         kind: "struct";
         fields: [
-          {
-            name: "authority";
-            type: "publicKey";
-          },
-          {
-            name: "score";
-            type: "u64";
-          },
-          {
-            name: "map";
-            type: "u8";
-          },
-          {
-            name: "health";
-            type: "u8";
-          },
-          {
-            name: "created";
-            type: "i64";
-          },
-          {
-            name: "updated";
-            type: "i64";
-          }
+          { name: "authority"; type: "publicKey" },
+          { name: "id"; type: "u8" },
+          { name: "quantity"; type: "u64" },
+          { name: "exports"; type: "u8" }
         ];
       };
     },
     {
-      name: "itemAccount";
+      name: "UserAccount";
       type: {
         kind: "struct";
         fields: [
-          {
-            name: "authority";
-            type: "publicKey";
-          },
-          {
-            name: "id";
-            type: "u8";
-          },
-          {
-            name: "quantity";
-            type: "u64";
-          },
-          {
-            name: "exports";
-            type: "u8";
-          }
+          { name: "authority"; type: "publicKey" },
+          { name: "score"; type: "u64" },
+          { name: "map"; type: "u8" },
+          { name: "health"; type: "u8" }
         ];
       };
     }
@@ -219,211 +84,76 @@ export const IDL: Dungeon3 = {
   name: "dungeon3",
   instructions: [
     {
-      name: "setUser",
+      name: "addItem",
       accounts: [
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "userAccount",
-          isMut: true,
-          isSigner: false,
-        },
+        { name: "authority", isMut: true, isSigner: true },
+        { name: "itemAccount", isMut: true, isSigner: false },
       ],
-      args: [
-        {
-          name: "score",
-          type: "u64",
-        },
-        {
-          name: "map",
-          type: "u8",
-        },
-        {
-          name: "health",
-          type: "u8",
-        },
-      ],
+      args: [],
     },
     {
       name: "initUser",
       accounts: [
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "newUserAccount",
-          isMut: true,
-          isSigner: false,
-          pda: {
-            seeds: [
-              {
-                kind: "const",
-                type: "string",
-                value: "user",
-              },
-              {
-                kind: "account",
-                type: "publicKey",
-                path: "authority",
-              },
-            ],
-          },
-        },
-        {
-          name: "rent",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "systemProgram",
-          isMut: false,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: "addItem",
-      accounts: [
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "itemAccount",
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: "exportItems",
-      accounts: [
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "itemAccount",
-          isMut: true,
-          isSigner: false,
-        },
+        { name: "authority", isMut: true, isSigner: true },
+        { name: "newUserAccount", isMut: true, isSigner: false },
+        { name: "systemProgram", isMut: false, isSigner: false },
+        { name: "rent", isMut: false, isSigner: false },
       ],
       args: [],
     },
     {
       name: "initItem",
       accounts: [
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "newItemAccount",
-          isMut: true,
-          isSigner: false,
-          pda: {
-            seeds: [
-              {
-                kind: "const",
-                type: "string",
-                value: "item",
-              },
-              {
-                kind: "account",
-                type: "publicKey",
-                path: "authority",
-              },
-              {
-                kind: "arg",
-                type: "u8",
-                path: "id",
-              },
-            ],
-          },
-        },
-        {
-          name: "rent",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "systemProgram",
-          isMut: false,
-          isSigner: false,
-        },
+        { name: "authority", isMut: true, isSigner: true },
+        { name: "newItemAccount", isMut: true, isSigner: false },
+        { name: "systemProgram", isMut: false, isSigner: false },
+        { name: "rent", isMut: false, isSigner: false },
+      ],
+      args: [{ name: "id", type: "u8" }],
+    },
+    {
+      name: "setUser",
+      accounts: [
+        { name: "authority", isMut: true, isSigner: true },
+        { name: "userAccount", isMut: true, isSigner: false },
       ],
       args: [
-        {
-          name: "id",
-          type: "u8",
-        },
+        { name: "score", type: "u64" },
+        { name: "map", type: "u8" },
+        { name: "health", type: "u8" },
       ],
+    },
+    {
+      name: "exportItems",
+      accounts: [
+        { name: "authority", isMut: true, isSigner: true },
+        { name: "itemAccount", isMut: true, isSigner: false },
+      ],
+      args: [],
     },
   ],
   accounts: [
     {
-      name: "userAccount",
+      name: "ItemAccount",
       type: {
         kind: "struct",
         fields: [
-          {
-            name: "authority",
-            type: "publicKey",
-          },
-          {
-            name: "score",
-            type: "u64",
-          },
-          {
-            name: "map",
-            type: "u8",
-          },
-          {
-            name: "health",
-            type: "u8",
-          },
-          {
-            name: "created",
-            type: "i64",
-          },
-          {
-            name: "updated",
-            type: "i64",
-          },
+          { name: "authority", type: "publicKey" },
+          { name: "id", type: "u8" },
+          { name: "quantity", type: "u64" },
+          { name: "exports", type: "u8" },
         ],
       },
     },
     {
-      name: "itemAccount",
+      name: "UserAccount",
       type: {
         kind: "struct",
         fields: [
-          {
-            name: "authority",
-            type: "publicKey",
-          },
-          {
-            name: "id",
-            type: "u8",
-          },
-          {
-            name: "quantity",
-            type: "u64",
-          },
-          {
-            name: "exports",
-            type: "u8",
-          },
+          { name: "authority", type: "publicKey" },
+          { name: "score", type: "u64" },
+          { name: "map", type: "u8" },
+          { name: "health", type: "u8" },
         ],
       },
     },
