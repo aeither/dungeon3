c6d609240fdc2bdf3f203c015b7bcac387a7974e
aeither <36173828+aeither@users.noreply.github.com>
2022-10-26 10:10:50 +0200

    use custom RPC

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,8 +1,5 @@
-import {
-  Adapter,
-  WalletAdapterNetwork,
-  WalletError,
-} from "@solana/wallet-adapter-base";
+import { NETWORK_URL } from "@/utils/constants";
+import { Adapter, WalletError } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -11,37 +8,26 @@ import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/walle
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import React, { FC, ReactNode, useCallback, useMemo } from "react";
+import { FC, ReactNode, useCallback } from "react";
 import { AutoConnectProvider, useAutoConnect } from "./AutoConnectProvider";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { autoConnect } = useAutoConnect();
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets: Adapter[] = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new SolletWalletAdapter({ network }),
-      new SolletExtensionWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-    ],
-    [network]
-  ) as any;
+  const wallets: Adapter[] = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new TorusWalletAdapter(),
+  ] as any;
 
   const onError = useCallback((error: WalletError) => {
     console.error(error);
   }, []);
 
   return (
-    // TODO: updates needed for updating and referencing endpoint: wallet adapter rework
-    <ConnectionProvider endpoint={endpoint}>
+    <ConnectionProvider endpoint={NETWORK_URL}>
       <WalletProvider
         wallets={wallets}
         onError={onError}
